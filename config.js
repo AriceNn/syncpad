f4e6a421d3049fd754a1a290c6060a243207a652
AriceNn <ertuertu1000@gmail.com>
2026-07-06 23:42:40 +0300

    Config: Update Vercel API URL

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+// Vercel Proxy Sunucu Adresiniz
+// Vercel'de projenizi yayınladıktan sonra size verilen adresi buraya yapıştırın.
+// Örnek: "https://my-clipboard-sync.vercel.app/api/sync"
+const VERCEL_API_URL = "https://clipboard-sync-extension.vercel.app/api/sync";
